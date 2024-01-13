8d669eafd5c02caacf94a9b7f4885d1985d1119b
guoqi.sun <sun412926@gmail.com>
2024-01-14 07:57:39 +0800

    :bug: fix: fix build error

--- vite.config.js
+++ vite.config.js
@@ -23,4 +23,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      external: /\/src\/data\/.*/,
+    },
+  },
 })
